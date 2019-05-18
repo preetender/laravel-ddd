3000a1e0873f22f0131d23a3d92e20e7d8d96077
Cesar Augusto <cesinhagutierres@gmail.com>
2019-05-18 14:55:18 -0400

    Veriicação de escopo

--- app/Units/Kernel/Http.php
+++ app/Units/Kernel/Http.php
@@ -60,6 +60,8 @@ class Http extends Kernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'scopes' => \Laravel\Passport\Http\Middleware\CheckScopes::class,
+        'scope' => \Laravel\Passport\Http\Middleware\CheckForAnyScope::class,
     ];
 
     /**
